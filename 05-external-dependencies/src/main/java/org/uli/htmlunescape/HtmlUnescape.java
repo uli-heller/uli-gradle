7f3f800e887a3424bd2d1ac1dedb7d78e0c826f0
Uli Heller <uli.heller@daemons-point.com>
2013-03-16 07:25:59 +0100

    Fixed - no encoding

--- 05-external-dependencies/src/main/java/org/uli/htmlunescape/HtmlUnescape.java
+++ 05-external-dependencies/src/main/java/org/uli/htmlunescape/HtmlUnescape.java
@@ -3,8 +3,8 @@ package org.uli.htmlunescape;
 import org.apache.commons.lang3.StringEscapeUtils;
 
 public class HtmlUnescape {
-	public String unescapeHtml(String src, String encoding) {
-		return StringEscapeUtils(src, encoding);
+	public String unescapeHtml(String src) {
+		return StringEscapeUtils.unescapeHtml4(src);
 	}
 }
 
