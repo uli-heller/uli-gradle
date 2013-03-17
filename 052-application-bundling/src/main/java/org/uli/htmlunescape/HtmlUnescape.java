28d81bb8ca8ceb4d7fbf6cdc52dd456a8dca660d
Uli Heller <uli.heller@daemons-point.com>
2013-03-17 07:12:46 +0100

    052: Use separate main class

--- 052-application-bundling/src/main/java/org/uli/htmlunescape/HtmlUnescape.java
+++ 052-application-bundling/src/main/java/org/uli/htmlunescape/HtmlUnescape.java
@@ -6,12 +6,5 @@ public class HtmlUnescape {
     public String unescapeHtml(String src) {
 	return StringEscapeUtils.unescapeHtml4(src);
     }
-
-    static public void main(String[] args) {
-	HtmlUnescape htmlUnescape = new HtmlUnescape();
-	for (String arg : args) {
-	    System.out.println(htmlUnescape.unescapeHtml(arg));
-	}
-    }
 }
 
