4d29d3bf898d71ec7e112bda816c62e52452dd94
Uli Heller <uli.heller@daemons-point.com>
2013-03-17 07:15:42 +0100

    052: Fixed eclipse warning - StringUtils

--- 052-application-bundling/src/main/java/org/uli/htmlunescape/Main.java
+++ 052-application-bundling/src/main/java/org/uli/htmlunescape/Main.java
@@ -1,13 +1,10 @@
 package org.uli.htmlunescape;
 
-import org.apache.commons.lang3.StringEscapeUtils;
-
 public class Main {
     static public void main(String[] args) {
-	HtmlUnescape htmlUnescape = new HtmlUnescape();
-	for (String arg : args) {
-	    System.out.println(htmlUnescape.unescapeHtml(arg));
-	}
+        HtmlUnescape htmlUnescape = new HtmlUnescape();
+        for (String arg : args) {
+            System.out.println(htmlUnescape.unescapeHtml(arg));
+        }
     }
 }
-
