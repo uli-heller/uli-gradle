3ee5cbec720ce03223af752e2dac80dafd271f31
Uli Heller <uli.heller@daemons-point.com>
2013-03-18 09:49:14 +0100

    Fixed indentation

--- 030-gradle-wrapper/src/main/java/org/uli/sha1/SHA1.java
+++ 030-gradle-wrapper/src/main/java/org/uli/sha1/SHA1.java
@@ -6,23 +6,24 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class SHA1 {
-   public String sha1hex(String input) throws NoSuchAlgorithmException, UnsupportedEncodingException {
-      byte[] bytesOfMessage = input.getBytes();
-      MessageDigest md = MessageDigest.getInstance("SHA1");
-      byte[] thedigest = md.digest(bytesOfMessage);
-      BigInteger bigInt = new BigInteger(1, thedigest);
-      String hashtext = bigInt.toString(16);
-      // Now we need to zero pad it if you actually want the full 32 chars.
-      while (hashtext.length() < 32) {
-         hashtext = "0" + hashtext;
-      }
-      return hashtext;
-   }
-   
-   static public void main(String[] args) throws Exception {
-       SHA1 sha1 = new SHA1(); 
-      for (String arg: args) {
-         System.out.println(sha1.sha1hex(arg));
-      }
-   }
+
+    public String sha1hex(String input) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        byte[] bytesOfMessage = input.getBytes();
+        MessageDigest md = MessageDigest.getInstance("SHA1");
+        byte[] thedigest = md.digest(bytesOfMessage);
+        BigInteger bigInt = new BigInteger(1, thedigest);
+        String hashtext = bigInt.toString(16);
+        // Now we need to zero pad it if you actually want the full 32 chars.
+        while (hashtext.length() < 32) {
+            hashtext = "0" + hashtext;
+        }
+        return hashtext;
+    }
+
+    static public void main(String[] args) throws Exception {
+        SHA1 sha1 = new SHA1();
+        for (String arg : args) {
+            System.out.println(sha1.sha1hex(arg));
+        }
+    }
 }
