eeec7ff38b1148ecce31250225081a0d3852ffdd
Uli Heller <uli.heller@daemons-point.com>
2013-03-18 09:13:41 +0100

    HttpCat -> MD5

---
+++ 020-java-quickstart-gradlew/src/main/java/org/uli/md5/MD5.java
@@ -0,0 +1,28 @@
+package org.uli.md5;
+
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MD5 {
+   public String md5hex(String input) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+      byte[] bytesOfMessage = input.getBytes();
+      MessageDigest md = MessageDigest.getInstance("MD5");
+      byte[] thedigest = md.digest(bytesOfMessage);
+      BigInteger bigInt = new BigInteger(1, thedigest);
+      String hashtext = bigInt.toString(16);
+      // Now we need to zero pad it if you actually want the full 32 chars.
+      while (hashtext.length() < 32) {
+         hashtext = "0" + hashtext;
+      }
+      return hashtext;
+   }
+   
+   static public void main(String[] args) throws Exception {
+       MD5 md5 = new MD5(); 
+      for (String arg: args) {
+         System.out.println(md5.md5hex(arg));
+      }
+   }
+}
