f01ebd43fc364ac6d5d378b75a4d5057b4ee3632
Uli Heller <uli.heller@daemons-point.com>
2013-03-18 08:09:57 +0100

    100: Added more tests, use in-memory db

--- 100-jpa/src/test/java/org/uli/jpa/PersonTest.java
+++ 100-jpa/src/test/java/org/uli/jpa/PersonTest.java
@@ -5,12 +5,17 @@ package org.uli.jpa;
 
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 
@@ -19,15 +24,21 @@ import org.junit.Test;
  *
  */
 public class PersonTest {
-    private static final String PERSISTENCE_UNIT_NAME = "jpa";
+    private static final String PERSISTENCE_UNIT_NAME = "jpa-test";
     private static EntityManagerFactory entityManagerFactory;
+
+    private static Map<Integer, Person> persons = new HashMap<Integer, Person>();
+
+    @BeforeClass
+    static public void initEm() {
+        entityManagerFactory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+    }
 
     /**
      * @throws java.lang.Exception
      */
     @Before
     public void setUp() throws Exception {
-        entityManagerFactory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
         EntityManager entityManager = entityManagerFactory.createEntityManager();
         Query query = entityManager.createQuery("select p from Person p");
 
@@ -39,6 +50,7 @@ public class PersonTest {
                 person.setFirstName("firstName-"+i);
                 person.setLastName("lastName-"+i);
                 entityManager.persist(person);
+                persons.put(person.getPersonId(), person);
             }
             entityManager.getTransaction().commit();
         }
@@ -49,8 +61,36 @@ public class PersonTest {
     public void testNotEmpty() {
         EntityManager entityManager = entityManagerFactory.createEntityManager();
         Query query = entityManager.createQuery("select p from Person p");
+        @SuppressWarnings("unchecked")
+        List<Object> resultList = query.getResultList();
+        assertFalse(resultList.isEmpty());
+        assertEquals(persons.size(), resultList.size());
+        entityManager.close();
+    }
+    
+    @Test
+    public void testFind() {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        for (Person p : persons.values()) {
+            Integer personId = p.getPersonId();
+            Person dbPerson = entityManager.find(Person.class, personId);
+            assertEquals("Person-"+personId+", personId:",  personId, dbPerson.getPersonId());
+            assertEquals("Person-"+personId+", firstName:", p.getFirstName(), dbPerson.getFirstName());
+            assertEquals("Person-"+personId+", lastName:",  p.getLastName(),  dbPerson.getLastName());
+        }
+        entityManager.close();
+    }
 
-        assertFalse(query.getResultList().isEmpty());
+    @Test
+    public void testGetReference() {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        for (Person p : persons.values()) {
+            Integer personId = p.getPersonId();
+            Person dbPerson = entityManager.getReference(Person.class, personId);
+            assertEquals("Person-"+personId+", personId:",  personId, dbPerson.getPersonId());
+            assertEquals("Person-"+personId+", firstName:", p.getFirstName(), dbPerson.getFirstName());
+            assertEquals("Person-"+personId+", lastName:",  p.getLastName(),  dbPerson.getLastName());
+        }
         entityManager.close();
     }
 }
