04d42cb62d5f5353e38442386fe0d68f51da6b8f
Uli Heller <uli.heller@daemons-point.com>
2013-04-06 06:55:29 +0200

    070: Added logging to source code

--- 070-slf4j-log4j/src/main/java/org/uli/logging/EchoServer.java
+++ 070-slf4j-log4j/src/main/java/org/uli/logging/EchoServer.java
@@ -1,7 +1,15 @@
 package org.uli.logging;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class EchoServer {
+    final Logger logger = LoggerFactory.getLogger(EchoServer.class);
+
     public String echo(String shoutOut) {
-	return shoutOut;
+	logger.debug("-> shoutOut={}", shoutOut);
+	String result = shoutOut;
+	logger.debug("<- result={}", result);
+	return result;
     }
 }
