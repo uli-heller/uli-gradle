640e70733ed4af539f18969fdefe553b3a42aa46
Uli Heller <uli.heller@daemons-point.com>
2013-06-06 13:23:04 +0200

    090: jsf tutorial from mkyong

---
+++ 090-jsf-webapp/src/main/java/org/uli/HelloBean.java
@@ -0,0 +1,19 @@
+package org.uli;
+ 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
+  
+@ManagedBean
+@SessionScoped
+public class HelloBean implements Serializable {
+  private static final long serialVersionUID = 1L;
+  private String name;
+
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+}
